d33c30d86eaadbff99cf868f149ed0246ed69dfc
bit-trade <is@uain.su>
2024-10-31 20:56:12 +0200

    установка зависимостей и конфиг подключения к БД

---
+++ config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    FSTR_DB_HOST: str
+    FSTR_DB_PORT: int
+    FSTR_DB_LOGIN: str
+    FSTR_DB_PASS: str
+
+    @property
+    def db_url(self):
+        # postgresql+psycopg://login:pass@localhost:5432/db_name
+        return f'postgresql+psycopg://{self.FSTR_DB_LOGIN}:{self.FSTR_DB_PASS}@{self.FSTR_DB_HOST}:{self.FSTR_DB_PORT}/public'
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+settings = Settings()
--- secret.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-# user = os.environ.get('NETUSER')
-# passwd = os.environ.get('NETPASSWD')
