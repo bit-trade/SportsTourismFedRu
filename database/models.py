bed9445246033593bf601eddd4b48cd020bcc087
bit-trade <is@uain.su>
2024-11-01 19:38:17 +0200

    проектирование БД: модели ORM

--- database/models.py
+++ database/models.py
@@ -1,13 +1,37 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 
-metadata_obj = MetaData()
+BaseModel = declarative_base()
 
-pereval_added = Table()
 
-pereval_areas = Table()
+class PerevalAdded(BaseModel):
+    __tablename__ = 'pereval_added'
+    id = Column(Integer, primary_key=True)
+    date_added = Column(DateTime)
+    raw_data = Column(JSONB)
+    images = Column(JSONB)
 
-pereval_images = Table()
 
-spr_activities_types = Table()
+class PerevalAreas(BaseModel):
+    __tablename__ = 'pereval_areas'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    id_parent = Column(Integer, ForeignKey('pereval_areas.id'), nullable=False)
+
+
+class PerevalImages(BaseModel):
+    __tablename__ = 'pereval_images'
+    id = Column(Integer, primary_key=True)
+    date_added = Column(DateTime, default=datetime.now)
+    img = Column(LargeBinary, nullable=False)
+
+
+class SprActivitiesTypes(BaseModel):
+    __tablename__ = 'spr_activities_types'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text)
 
