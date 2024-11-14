a692409dd381f85f4d548c7042b73f45cf76f409
bit-trade <is@uain.su>
2024-11-14 20:10:28 +0200

    функция update_pass: first iteration

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from typing import Annotated
+
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from core.schemas import PassAdded
 from database.connect import get_db
-from database.models import  PerevalAdded
+from database.models import PerevalAdded, StatusPass
 
 pereval = FastAPI()
 
@@ -17,12 +19,11 @@ def get_home(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
 
 @pereval.post('/submitData')
 def submit_data(passage: PassAdded, db: Session = Depends(get_db)):
-    pereval = PerevalAdded(raw_data=passage.raw_data, images=passage.images, moder_status=passage.moder_status)
+    pereval = PerevalAdded(raw_data=passage.raw_data, images=passage.images)
     db.add(pereval)
     db.commit()
     db.refresh(pereval)
     return pereval
-
 
 @pereval.get('/submitData/{pass_id}')
 def get_pass(pass_id: int, db: Session = Depends(get_db)):
@@ -31,3 +32,22 @@ def get_pass(pass_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Passage not found")
 
     return pereval
+
+@pereval.patch('/submitData/{pass_id}')
+def update_pass(pass_id: int, passage: Annotated[PassAdded, Depends()] , db: Session = Depends(get_db)):
+    pereval = db.get(PerevalAdded, pass_id)
+    if not pereval:
+        raise HTTPException(status_code=404, detail="Passage not found")
+
+    print('\n', pereval, type(pereval), '\n')
+    print(f'raw_data is {pereval.raw_data}' , type(pereval.raw_data), '\n')
+    print(f'moder_status is {pereval.moder_status}', type(pereval.moder_status), '\n')
+
+    if pereval.moder_status == StatusPass.new:
+        pereval = PerevalAdded(date_added=passage.data_added, raw_data=passage.raw_data,
+                               images=passage.images, moder_status=passage.moder_status)
+        db.add(pereval) # !!!!
+        db.commit()
+        db.refresh(pereval)
+        return pereval
+
